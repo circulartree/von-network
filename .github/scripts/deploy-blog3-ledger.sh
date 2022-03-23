de24ba0fbf1972d3c2cfd2affa410519a40ff3b0
Elias Strehle <elias@circulartree.com>
2022-03-23 09:55:58 +0100

    bug: pass secrets to server script

--- .github/scripts/deploy-blog3-ledger.sh
+++ .github/scripts/deploy-blog3-ledger.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-# cd /home/ubuntu/von-network
+USAGE="Usage: $(basename "$0") seed_1 seed_2
+Deploy demo ledger
 
-# ./manage rm
+where:
+  seed_1  Wallet seed for demo connector 1
+  seed_2  Wallet seed for demo connector 2"
 
-# ./manage build
-# ./manage up --wait
+if [ $# -lt 2 ]; then
+  echo 1>&2 "$0: missing arguments"
+  echo "$USAGE" >&2
+  exit 2
+fi
+DEMO_CONNECTOR_1_SEED=$1
+DEMO_CONNECTOR_2_SEED=$2
+
+cd /home/ubuntu/von-network
+
+./manage rm
+
+./manage build
+./manage up --wait
 
 echo "Registering DIDs" >&2
-echo "${DEMO_CONNECTOR_1_SEED}" >&2
-echo "${DEMO_CONNECTOR_2_SEED}" >&2
-# until $(curl --output /dev/null --silent --fail -X POST http://localhost:9000/register -H "Content-Type: application/json" -d '{"did": null, "seed": "'${DEMO_CONNECTOR_1_SEED}'"}'); do
-#     printf "."
-#     sleep 1
-# done
-# until $(curl --output /dev/null --silent --fail -X POST http://localhost:9000/register -H "Content-Type: application/json" -d '{"did": null, "seed": "'${DEMO_CONNECTOR_2_SEED}'"}'); do
-#     printf "."
-#     sleep 1
-# done
+until $(curl --output /dev/null --silent --fail -X POST http://localhost:9000/register -H "Content-Type: application/json" -d '{"did": null, "seed": "'${DEMO_CONNECTOR_1_SEED}'"}'); do
+    printf "."
+    sleep 1
+done
+until $(curl --output /dev/null --silent --fail -X POST http://localhost:9000/register -H "Content-Type: application/json" -d '{"did": null, "seed": "'${DEMO_CONNECTOR_2_SEED}'"}'); do
+    printf "."
+    sleep 1
+done
