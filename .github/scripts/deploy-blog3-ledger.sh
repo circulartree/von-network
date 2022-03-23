52b0d14cc8ff717967c0fa0170cee09b24cad2f6
Elias Strehle <elias@circulartree.com>
2022-03-23 10:09:57 +0100

    add test seed

--- .github/scripts/deploy-blog3-ledger.sh
+++ .github/scripts/deploy-blog3-ledger.sh
@@ -4,16 +4,18 @@ USAGE="Usage: $(basename "$0") seed_1 seed_2
 Deploy demo ledger
 
 where:
+  seed_0  Demo seed
   seed_1  Wallet seed for demo connector 1
   seed_2  Wallet seed for demo connector 2"
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 3 ]; then
   echo 1>&2 "$0: missing arguments"
   echo "$USAGE" >&2
   exit 2
 fi
-DEMO_CONNECTOR_1_SEED=$1
-DEMO_CONNECTOR_2_SEED=$2
+DEMO_TEST_SEED=$1
+DEMO_CONNECTOR_1_SEED=$2
+DEMO_CONNECTOR_2_SEED=$3
 
 cd /home/ubuntu/von-network
 
@@ -23,10 +25,17 @@ cd /home/ubuntu/von-network
 ./manage up --wait
 
 echo "Registering DIDs" >&2
+# test seed
+until $(curl --output /dev/null --silent --fail -X POST http://localhost:9000/register -H "Content-Type: application/json" -d '{"did": null, "seed": "'${DEMO_TEST_SEED}'"}'); do
+    printf "."
+    sleep 1
+done
+# demo connector 1 seed
 until $(curl --output /dev/null --silent --fail -X POST http://localhost:9000/register -H "Content-Type: application/json" -d '{"did": null, "seed": "'${DEMO_CONNECTOR_1_SEED}'"}'); do
     printf "."
     sleep 1
 done
+# demo connector 2 seed
 until $(curl --output /dev/null --silent --fail -X POST http://localhost:9000/register -H "Content-Type: application/json" -d '{"did": null, "seed": "'${DEMO_CONNECTOR_2_SEED}'"}'); do
     printf "."
     sleep 1
