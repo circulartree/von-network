be1d581bff62445d9d0b8b082c32725c34f7b7a0
Elias Strehle <elias@circulartree.com>
2022-03-23 09:47:35 +0100

    debug secrets in script

--- .github/scripts/deploy-blog3-ledger.sh
+++ .github/scripts/deploy-blog3-ledger.sh
@@ -7,12 +7,14 @@ cd /home/ubuntu/von-network
 ./manage build
 ./manage up --wait
 
-printf "Registering DIDs" >&2
-until $(curl --output /dev/null --silent --fail -X POST http://localhost:9000/register -H "Content-Type: application/json" -d '{"did": null, "seed": "'${DEMO_CONNECTOR_1_SEED}'"}'); do
-    printf "."
-    sleep 1
-done
-until $(curl --output /dev/null --silent --fail -X POST http://localhost:9000/register -H "Content-Type: application/json" -d '{"did": null, "seed": "'${DEMO_CONNECTOR_2_SEED}'"}'); do
-    printf "."
-    sleep 1
-done
+echo "Registering DIDs" >&2
+echo ${DEMO_CONNECTOR_1_SEED}
+echo ${DEMO_CONNECTOR_2_SEED}
+# until $(curl --output /dev/null --silent --fail -X POST http://localhost:9000/register -H "Content-Type: application/json" -d '{"did": null, "seed": "'${DEMO_CONNECTOR_1_SEED}'"}'); do
+#     printf "."
+#     sleep 1
+# done
+# until $(curl --output /dev/null --silent --fail -X POST http://localhost:9000/register -H "Content-Type: application/json" -d '{"did": null, "seed": "'${DEMO_CONNECTOR_2_SEED}'"}'); do
+#     printf "."
+#     sleep 1
+# done
