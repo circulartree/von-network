017a6f45df16f6a7870e3f0d771e601fc53466fe
Elias Strehle <elias@circulartree.com>
2022-03-25 16:47:09 +0100

    fail after 5 retries to avoid non-termination

--- .github/scripts/blog3-register-dids.sh
+++ .github/scripts/blog3-register-dids.sh
@@ -17,8 +17,12 @@ SEEDS_FILE=$1
 echo "Registering DIDs" >&2
 while read -r seed
 do
+  max_retry=10
+  counter=0
   until $(curl --output /dev/null --silent --fail -X POST http://localhost:9000/register -H "Content-Type: application/json" -d '{"did": null, "seed": "'${seed}'"}'); do
     printf "."
     sleep 1
+    [[ counter -eq $max_retry ]] && curl -X POST http://localhost:9000/register -H "Content-Type: application/json" -d '{"did": null, "seed": "'${seed}'"}'
+    (( counter++ ))
   done
 done < "${SEEDS_FILE}"
